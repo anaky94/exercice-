65352342f6fb234ea6ceb32096566cf061d53779
anaky94 <anaky.44@gmail.com>
2023-02-15 07:30:51 +0100

    learn_shell

--- index.sh
+++ index.sh
@@ -4,6 +4,25 @@
 # result=$((45+23))
 #number=$((96 + 4444))
 #echo $number 
-if test $b1 < $nb2 ; then 
+
+
+# bourne (sh) --> bash, csh, zsh, ksh 
+# - lt, -l 
+# -gt, -ge
+# -eq, -ne
+# =, !=
+# -e : existe dans le répertoire courant 
+# -f : si c'est un fichier 
+# -d : si c'est un répertoire 
+
+nb1='hello'
+nb2='world'
+nb3='world'
+
+if [ $nb1 -le $nb2] ; then 
     echo 'nb1 < nb2' 
+elif [ $nb1 -gt $nb2] ; then 
+    echo 'nb1> nb2'
+else 
+    echo 'nb1 = nb2'
 fi
