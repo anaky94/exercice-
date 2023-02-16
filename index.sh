9122a043da359c6df40fb6322efcd830133ce0b4
anaky94 <anaky.44@gmail.com>
2023-02-16 23:26:08 +0100

    4envoie

--- index.sh
+++ index.sh
@@ -7,12 +7,19 @@
 
 
 # bourne (sh) --> bash, csh, zsh, ksh 
-# - lt, -l 
+# 
+# - lt, -le 
 # -gt, -ge
 # -eq, -ne
 # =, !=
 # -e : existe dans le répertoire courant 
 # -f : si c'est un fichier 
 # -d : si c'est un répertoire 
+# Quelque option utile de la commande read -p : afficher un message, -n limiter le nombre de caractères, -t : limiter le temps autorisé pour saisir un message, -s : ne pas afficher 
 
 
+
+
+# Learn Variable
+
+#learn conditions
