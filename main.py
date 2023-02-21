b352d92cd8e19457415b37d7387ebf292697661b
anaky94 <anaky.44@gmail.com>
2023-02-21 06:40:03 +0100

    learn python

--- main.py
+++ main.py
@@ -13,23 +13,41 @@
 """
 # afficher_information_personne
 # parametre: nom, age 
-def afficher_informations_personne(nom, age):
-    print("vous vous appeleez " + nom + ", vous avez"+str(age) + " ans ")
-    print("l'an prochain vous aurez " + str(age+1)+"ans")
-
+def afficher_informations_personne(nom, age, taille=0):
+    print()
+    print("vous vous appeleez " + nom + ", vous avez "+str(age) + " ans ")
+    print("l'an prochain vous aurez " + str(age+1)+" ans")
 # == égal 
 # < inférieur 
 # <= inérieur ou égal
 # > supérieur 
 # >=supérieur ou égal
-
-#age == 17
-#
-if age == 18:
-    print("vous etes majeur")
-else:
-    print("vous êtes mineur")
-
+# true / False (boolean)
+# elif --> else /if 
+# age > 60 : vous etes sénior
+#age < 10 : vous êtes enfant
+# ado si age >= 12 et age < 18 
+# bebe si age == 1 ou age 2 
+    
+    if age == 17:
+        print("vous êtes presque majeur")
+    elif 12 <=  age < 18:
+        print("vous etes ado")
+    elif age == 1 or age == 2:
+        print("vous êtes un bebe")
+    elif age == 18:
+        print("vous êtes juste majeur")
+    elif age > 60:
+        print("vous êtes sénior")
+    elif age < 10:
+        print("vous êtes enfant")
+    elif age >= 18:
+        print("vous etes majeurs")
+    else:
+        print("vous êtes mineur")
+    #afficher la taille 
+    
+    print("votres taille : "+ str(taille) + "m")
 
 
 def demander_nom():
@@ -53,17 +71,19 @@ def demander_age(nom_personne):
 #=====================================================================>
 
 #demander le nom
-nom1 = demander_nom()
-nom2 = demander_nom()
+#nom1 = demander_nom()
+#nom2 = demander_nom()
 
+#nom1 = "personne1"
+#nom2 = "personne2"
 
 #demander l'age
-age1 = demander_age(nom1)
-age2 = demander_age(nom2)
+#age1 = demander_age(nom1)
+#age2 = demander_age(nom2)
 
 #afficher les resultats
-afficher_informations_personne(nom1, age1)
-afficher_informations_personne(nom2, age2)
+#afficher_informations_personne(nom1, age1)
+#afficher_informations_personne(nom2, age2)
 
 
 #n = 0 # cree la variable 
@@ -74,7 +94,12 @@ afficher_informations_personne(nom2, age2)
 #n = n + 1 # incrémenter
 #print(n)
 
- 
+ #la boucle for 
+NB_PERSONNES = 1
+for i in range(0,NB_PERSONNES):
+    nom = "personne " + str(i+1)
+    age = demander_age(nom)
+    afficher_informations_personne(nom, age, 1.89)
 
 
 
