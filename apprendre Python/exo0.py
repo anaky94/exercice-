e3217de991ba6da04a94b474f6fb40a4792fb55a
anaky94 <anaky.44@gmail.com>
2023-03-14 09:45:06 +0100

    exo

--- apprendre Python/exo0.py
+++ apprendre Python/exo0.py
@@ -1,6 +1,18 @@
 # ecrire un programme qui affiche une suite de 12 nombre dont chaque terme soit egal au triple du terme precedent 
-
+"""
 a, c = 1, 1
 while c <= 13:
     print(a, end=" ")
-    a, c = a*3, c+1
+    a, c = a*3, c+1"""
+
+#ecrire un programme qui demande un nombre de départ et qui ensuite affiche dix nombre suivant en utilisant la boucle for. 
+#ex: si l'utilisateur entre le nombre 33 le programme affichera les nombre de 34 à 43
+
+a=int(input("entré un nombre:"))
+for i in range (10):
+    a=a+1
+    print(a)
+  
+
+
+
--- apprendre Python/exo2.py
+++ apprendre Python/exo2.py
@@ -2,11 +2,11 @@
 """
 1: ecrire un programe en python pour asavoir combien il aura au bout de 10 ans 
  le 1er janvier, les parents de Gaspard ont déposé sur son livret d'épargne 1500€ au taux de d'interet de 2% annuel
-
-s = 1500
-
-for  i in range(0, 10):
-    s= s*float(1,02)
-    print(i,s)
-    
 """
+s = int(input("entrez un chiffre"))
+anne=10
+taux=0,2
+
+for i in  range(10):
+    i=i+1
+    print(i)
