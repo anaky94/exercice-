37f74db124136b5f1e8c3f8dab0d499ecad3d8fc
anaky94 <anaky.44@gmail.com>
2023-03-22 23:33:43 +0100

    exo

--- apprendre Python/fiche.py
+++ apprendre Python/fiche.py
@@ -1,5 +1,19 @@
-nom=input("quel est votre nom ? ")
-age=int(input("quel est votre age ? "))
-print("vous vous apeler "+nom + "et vos avez "+str(age)+"ans")
+
+#On demande le nom et l'age de la personne
+# Premier programme
+
+nom = input("quel est votre nom ? ")
+age = int(input("quel est votre age ? "))
+
+try:
+    age_prochain = int(age) + 1
+except:
+    print("Erreur Vous devez rentrer un nombre pour l'age")
+
+
+print("vous vous apeler " + nom + " et vos avez "+str(age)+" ans")
+print("vous aurez "+str(age_prochain)+" ans l'année prochaine")
+
+
 
 
