498380f3cc7b934858739ce812e85719c74077e0
anaky94 <anaky.44@gmail.com>
2023-03-23 08:34:18 +0100

    exo

--- apprendre Python/fiche.py
+++ apprendre Python/fiche.py
@@ -3,16 +3,28 @@
 # Premier programme
 
 nom = input("quel est votre nom ? ")
-age = int(input("quel est votre age ? "))
-
-try:
-    age_prochain = int(age) + 1
-except:
-    print("Erreur Vous devez rentrer un nombre pour l'age")
+age = input("quel est votre age ? ")
 
 
-print("vous vous apeler " + nom + " et vos avez "+str(age)+" ans")
-print("vous aurez "+str(age_prochain)+" ans l'année prochaine")
+age_prochain=0
+while age_prochain==0:
+    print("Quel est votre age? ")
+    try:
+        age_prochain = int(age) + 1
+    except ValueError :
+        print("Erreur: Vous devez rentrer un nombre pour l'age")
+  
+print("fin de la boucle")
+#print("vous vous apeler " + nom + " et vos avez "+str(age)+" ans")
+#print("vous aurez "+str(age_prochain)+" ans l'année prochaine")
+
+
+
+
+
+
+
+
 
 
 
