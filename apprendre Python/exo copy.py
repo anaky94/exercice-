2083e76c5d464d55d42ecb96fdc50c94f55c108b
anaky94 <anaky.44@gmail.com>
2023-05-01 17:47:59 +0200

    exo1

--- apprendre Python/exo copy.py
+++ apprendre Python/exo copy.py
@@ -1,48 +1,23 @@
-import random
 
-def demander_nombre(nb_min, nb_max):
-    nombre_int= 0 
-    while nombre_int == 0:
-        nombre_str = input(f"quel est le nombre magigique entre {nb_min} et {nb_max} ? ")
-        try:   
-            nombre_int=int(nombre_str)
+#demander nom
+nom=""
+while nom=="":
+    non=input("Quel est votre nom ?")
+
+
+# demander age 
+def demander_age():
+    age_str=0
+    while  age_str==0:
+        age_str=input("quel est votre age ? ")
+                
+        try:
+            age=int(age_str)
         except:
-            print("Erreur: vous devez rentrer un nombre. Réesayer ")
-        else:
-            if nombre_int < nb_min or nombre_int > nb_max:
-                print(f"Erreur: Le nombre doit être entre {nb_min} et {nb_min}. Reésayer")
-                nombre_int=0
-    return nombre_int
+            print("erreur ")
+    
 
-
-NOMBRE_MIN = 1
-NOMBRE_MAX=10
-NOMBRE_MAGIQUE= random.randint(NOMBRE_MIN, NOMBRE_MAX)
-nombre=0
-NB_VIES=4
-vies=NB_VIES
-
-
-while not nombre == NOMBRE_MAGIQUE and vies >0 :
-    print( f"Il vous reste {vies} vies ")
-    nombre = demander_nombre (NOMBRE_MIN,NOMBRE_MAX)
-    if nombre == NOMBRE_MAGIQUE:
-        print("bravo vous avez gagner !")
-        print (f"Il vous restait {vies} vies " )
-    elif nombre > NOMBRE_MAGIQUE:
-        print("le nombre est plus petit")
-        vies -=1
-    else:
-        print("le nombre est plus grand ")
-        vies -=1
-if vies==0:
-    print(f"Vous avez perdu ! le nombre magique etais : {NOMBRE_MAGIQUE} ")
-
-
-
-
-
-
+demander_age()
 
 
 
