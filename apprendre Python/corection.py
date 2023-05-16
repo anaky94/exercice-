6e1b6b6c869d34eb9ff6d20cde98700c0fb6d2d6
anaky94 <anaky.44@gmail.com>
2023-05-16 23:57:43 +0200

    corection convertion

--- apprendre Python/corection.py
+++ apprendre Python/corection.py
@@ -2,23 +2,24 @@
 #def effectuer_convertion(unit1: str, unit2: str, facteur: float):
 #   valeur_str = input(f" convertion{unit1}->  ")
 
+def effectuer_convertion(unit1 , unit2, facteur):  
+    valeur_str=input(f"Conversion {unit1} -> {unit2}. donnez la valeur en {unit1} (ou 'q' pour quitter): ")
+    if valeur_str == "q": 
+        return True
+    valeur_float=float(valeur_str)
+    valeur_convertie = round(valeur_float *2.54, 2)
+    print(f"Resultat de la convertion: {valeur_float} {unit1} = {valeur_convertie}  {unit2}")
+
+
 
 print("ce programme vous permet d'effectuer des conversions d'unitées ")
 print("1-pouces vers cm ")
 print("2-cm vers pouces ")
 choix=input("votre choix (1 ou 2): ")
-if choix =="1":
-    valeur_str=input("Conversion vers cm. donnez la valeur en pouces: ")
-    valeur_float=float(valeur_str)
-    valeur_convertie = round(valeur_float *2.54, 2)
-    print(f"Resultat de la convertion: {valeur_float} pouces = {valeur_convertie} cm ")
 
-   def effectuer_convertion("pouces", "cm", 2.54):
-
-if choix =="2":
-    valeur_str=input("Conversion vers pouces. donnez la valeur en cm: ")
-    valeur_float=float(valeur_str)
-    valeur_convertie = round(valeur_float *0.394, 2)
-    print(f"Resultat de la convertion: {valeur_float} cm = {valeur_convertie} pouces ")
+while True:
+    if choix=="1":
+        effectuer_convertion("pouces", "cm", 2.54)
+    if choix =="2":
+        effectuer_convertion("cm", "pouces", 0.394)
     
-    def effectuer_convertion("pouces", "cm", 2.54):
