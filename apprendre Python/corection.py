81c916a046e3832fd10c62803724374d1b14dc27
anaky94 <anaky.44@gmail.com>
2023-05-20 11:11:44 +0200

    exo

--- apprendre Python/corection.py
+++ apprendre Python/corection.py
@@ -2,13 +2,25 @@
 #def effectuer_convertion(unit1: str, unit2: str, facteur: float):
 #   valeur_str = input(f" convertion{unit1}->  ")
 
-def effectuer_convertion(unit1 , unit2, facteur):  
+"""
+True: l'utilisateur ne veut plus convertis 
+False; L'utilisateur a donné une valeur a convertir
+"""
+def effectuer_convertion(unit1:str , unit2: str, facteur: float):  
     valeur_str=input(f"Conversion {unit1} -> {unit2}. donnez la valeur en {unit1} (ou 'q' pour quitter): ")
     if valeur_str == "q": 
+        print("vous avez quitter")
         return True
-    valeur_float=float(valeur_str)
+    try:
+        valeur_float=float(valeur_str)
+    except ValueError:
+        print("erreur rentré une valeur numérique\n utilisez le '.' et non une virgule ")
+        print()
+        return effectuer_convertion(unit1, unit2, facteur)
+
     valeur_convertie = round(valeur_float *2.54, 2)
     print(f"Resultat de la convertion: {valeur_float} {unit1} = {valeur_convertie}  {unit2}")
+    return False
 
 
 
@@ -19,7 +31,9 @@ choix=input("votre choix (1 ou 2): ")
 
 while True:
     if choix=="1":
-        effectuer_convertion("pouces", "cm", 2.54)
+       if effectuer_convertion("pouces", "cm", 2.54):
+            break
     if choix =="2":
-        effectuer_convertion("cm", "pouces", 0.394)
+        if effectuer_convertion("cm", "pouces", 0.394):
+            break
     
