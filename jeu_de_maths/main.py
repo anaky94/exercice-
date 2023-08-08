272447685c9e923a07ffec996facc3cac0671427
anaky94 <anaky.44@gmail.com>
2023-08-08 21:54:10 +0200

    exo

--- jeu_de_maths/main.py
+++ jeu_de_maths/main.py
@@ -4,9 +4,10 @@
 a=NOMBRE_MIN=1
 b=NOMBRE_MAX= 10 
 NB_QUESTION =4
+moyenne=2,5
 
 def poser_question():
-   reponse_str= input(f"calculer  : {a} + {b}=")
+   reponse_str= input(f"calculer  : {a} + {b} =")
    reponse_int=int(reponse_str)
    if reponse_int == a+b:
       return True
@@ -25,7 +26,19 @@ for i in range(0, NB_QUESTION):
       print("reponse incorecte")
    print()
 
+print( f"votre note est {NB_points} /{NB_QUESTION}")
+if NB_points == NB_QUESTION:
+   print("Ecxellent")
+elif NB_points ==0:
+   print("Réviser vos Maths")
+elif NB_points> moyenne:
 
+# votre note est de 2/4 -> Excellent!
+# 0 --> réviser vos maths!
+# moyenne --> int(NB_QUESTION) 2.5 --> é 
+# < moyenne --> peut mieux faire 
+
+print("fin de la partie")
 
 
  
