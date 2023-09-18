0df3104dec21d3646eea4a67544b6fe906331f3a
anaky94 <anaky.44@gmail.com>
2023-09-18 10:11:38 +0200

    site web pro

--- apprendre Python/correction.py
+++ apprendre Python/correction.py
@@ -5,12 +5,21 @@ voici comment le programme doit se comporter :
 3: afficher la valeur convertir 
  """
 
+# convertion de unit1 vers unit2  
+def effectuer_convertion(unit1, unit2, facteur):
+    valeur_str=input( f"convertion {unit1} -> {unit2} . Donnez la valeur en pouces: {unit1} (ou 'q' pour quitter) ")
+    if valeur_str=="q":
+        exit()
+    valeur_float=float(valeur_str)
+    valeur_convertie = round(valeur_float * facteur, 2)
+    print(f" Résultat de la convertion {valeur_float} {unit1}= {valeur_convertie} {unit2}")
+
 print("ce programme vous permet d'effectuer des convertion d'unité")
 print("1 - pouces vers cm " )
 print("2 cm vers pouces")
 choice = input("choix (1 ou 2): " )
-if choice=="1":
-    valeur_str=input( "convertion pouces -> cm. Donnez la valeur en pouces:  ")
-    valeur_float=float(valeur_str)
-    valeur_convertie=valeur_float*2.5
-    print(f" Résultat de la convertion {valeur_float} pouces= {valeur_convertie} cm")
+while True: 
+    if choice=="1":
+        effectuer_convertion("pouce", "cm", 2.54)
+    if choice=="2":
+        effectuer_convertion("cm","pouces", 0.394)
